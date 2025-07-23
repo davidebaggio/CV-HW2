d93453144175cef7adb074e90f8bc5132bdd2148
ThePelatz <zorenmartinez5@gmail.com>
2025-07-23 16:50:58 +0200

    modified build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cmake -B build
+if [ ! -d "libtorch" ]; then
+    wget https://download.pytorch.org/libtorch/test/cpu/libtorch-cxx11-abi-shared-with-deps-latest.zip
+    unzip libtorch-cxx11-abi-shared-with-deps-latest.zip
+    rm libtorch-cxx11-abi-shared-with-deps-latest.zip
+fi
 
+cmake -B build
 make -j6 -C build
