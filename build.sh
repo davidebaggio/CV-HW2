d99980cac63466a088f547da8cd6bdbf5b78f842
davidebaggio <davide.davide2405@gmail.com>
2025-08-02 18:55:38 +0200

    new build

--- build.sh
+++ build.sh
@@ -7,10 +7,14 @@ if [ ! -d "libtorch" ]; then
     rm libtorch-cxx11-abi-shared-with-deps-latest.zip
 fi
 
-
 # Download nlohmann/json.hpp into include/ if not already present
 if [ ! -f "include/json.hpp" ]; then
     wget https://github.com/nlohmann/json/releases/download/v3.11.3/json.hpp -O include/json.hpp
+fi
+
+# Download our trained model if not already present
+if [ ! -f "simple_card_classifier_traced.pt" ]; then
+    wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1jSCMt6EG0g56-9XafxIkm-zRKy2Y-9aP" -O simple_card_classifier_traced.pt
 fi
 
 # Build the project
